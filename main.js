01a3fa0bba56793a789a62a4cdfedf04a9f3f90a
Рашов Евгений <hakerovski1992@gmail.com>
2022-05-26 19:29:34 +0300

    comment

---
+++ main.js
@@ -0,0 +1,40 @@
+let money = +prompt('Ваш бюджет на месяц?', '');
+    time = prompt(' Введите дату в фомате YYYY-MM-DD', '');
+
+let appData = {
+    budget: money,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    timeData: time,
+    savings: false
+};
+
+
+
+ for (let i = 0; i<2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = prompt("Во сколько обойдется", '');
+    if ((typeof(a)) === 'string' &&((typeof(a)) !== null) && (typeof(b)) !== null
+    && a !== '' &&  b !== '' && a.lenght < 50) {
+        appData.expenses[a] = b;
+    }else {
+        console.log("Повтори заново")
+
+    }
+    
+ };
+ appData.moneyPerDay = appData.budget / 30;
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if(appData.moneyPerDay < 100) {
+    console.log("Минимальный уровеь достатка")
+}else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровеь достатка")
+}else if (appData.moneyPerDay > 2000) {
+    console.log("Хороший  уровеь достатка")
+} else {
+    console.log("Произошла ошибка")
+}
+
+
